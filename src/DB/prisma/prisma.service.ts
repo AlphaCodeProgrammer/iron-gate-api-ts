37e06d36144844bfbd1c65b659ab4dc2ccc7de85
Alpha <alphacodeprogrammer@gmail.com>
2025-06-10 20:24:28 +0330

    Redis added to project by all configurations

---
+++ src/DB/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    try{
+      await this.$connect();
+      console.log('✅Prisma Connected to the database successfully');
+    } catch (error) {
+      console.log('❌Prisma Connection failed', );
+    }
+  }
+
+  async onModuleDestroy() {
+    try {
+      await this.$disconnect();
+      console.log('🔌 Prisma Disconnected from the database');
+    } catch (error) {
+      console.log('❌Prisma Disconnection failed');
+    }
+  }
+}
